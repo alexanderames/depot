b7d63abbfc71e86bc6f43680a5753c78f93f2377
alexander <=>
2020-10-14 09:44:42 -0600

    RUBOCOP

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:new, :create]
+  before_action :set_cart, only: %i[new create]
   before_action :ensure_cart_isnt_empty, only: :new
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_order, only: %i[show edit update destroy]
 
   # GET /orders
   # GET /orders.json
@@ -12,8 +12,7 @@ class OrdersController < ApplicationController
 
   # GET /orders/1
   # GET /orders/1.json
-  def show
-  end
+  def show; end
 
   # GET /orders/new
   def new
@@ -21,8 +20,7 @@ class OrdersController < ApplicationController
   end
 
   # GET /orders/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /orders
   # POST /orders.json
@@ -34,14 +32,20 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        format.html { redirect_to store_index_url, notice: 
-          'Thank you for your order.' }
-        format.json { render :show, status: :created,
-          location: @order }
+        format.html do
+          redirect_to store_index_url, notice:
+          'Thank you for your order.'
+        end
+        format.json do
+          render :show, status: :created,
+                        location: @order
+        end
       else
         format.html { render :new }
-        format.json { render json: @order.errors,
-          status: :unprocessable_entity }
+        format.json do
+          render json: @order.errors,
+                 status: :unprocessable_entity
+        end
       end
     end
   end
@@ -71,32 +75,33 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.require(:order).permit(:name, :address, :email, :pay_type)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
 
-    def ensure_cart_isnt_empty
-      if @cart.line_items.empty?
-        redirect_to store_index_url, notice: 'Your cart is empty'
-      end
-    end
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:name, :address, :email, :pay_type)
+  end
 
-    # Explicit params for payment type
-    def pay_type_params
-      if order_params[:pay_type] == "Credit card"
-        params.require(:order).permit(:credit_card_number, :expiration_date)
-      elsif order_params[:pay_type] == "Check"
-        params.require(:order).permit(:routing_number, :account_number)
-      elsif order_params[:pay_type] == "Purchase order"
-        params.require(:order).permit(:po_number)
-      else
-        {}
-      end
+  def ensure_cart_isnt_empty
+    if @cart.line_items.empty?
+      redirect_to store_index_url, notice: 'Your cart is empty'
     end
+  end
+
+  # Explicit params for payment type
+  def pay_type_params
+    if order_params[:pay_type] == 'Credit card'
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    elsif order_params[:pay_type] == 'Check'
+      params.require(:order).permit(:routing_number, :account_number)
+    elsif order_params[:pay_type] == 'Purchase order'
+      params.require(:order).permit(:po_number)
+    else
+      {}
+    end
+  end
 end
